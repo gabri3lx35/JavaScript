a48284d71996f721f06547da9d3288c0c9206ccf
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2023-07-16 15:46:21 -0300

    Initial commit

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 55
+console.log(`A Velocidade do seu caro é ${vel}Km/h`)
+if (vel >= 60){
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
