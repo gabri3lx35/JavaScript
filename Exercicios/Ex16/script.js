9eb5aa57c95eb50a9e7341a82aedd8c7d7219b37
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2023-09-20 11:25:56 -0300

    testando inputs

---
+++ Exercicios/Ex16/script.js
@@ -0,0 +1,12 @@
+function contar(){
+    let i = document.getElementById('N-inicio')
+    let f = document.getElementById('N-final')
+    let p = document.getElementById('N-passo')
+
+    let res = document.getElementById('res')
+    if(i || f || p == 0){
+       alert('ERRO')
+    }else{
+        alert('gg')
+    }
+}
