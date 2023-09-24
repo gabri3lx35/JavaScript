228a1c7e34837a18830b829f691bb141588857cf
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2023-09-23 22:02:28 -0300

    Erro Arumado

--- Exercicios/Ex16/script.js
+++ Exercicios/Ex16/script.js
@@ -2,15 +2,30 @@ function contar(){
     let i = document.getElementById('txti')
     let f = document.getElementById('txtf')
     let p = document.getElementById('txtp')
-
     let res = document.getElementById('res')
 
-    if(i.value.lenght == 0 || f.value.lenght == 0 || p.value.lenght == 0){
-        window.alert('ERRO falta dados')
+    if(i.value.lenght == 0 || f.value.length == 0 || p.value.length == 0){
+        res.innerHTML = 'Imporsivel Contar'
+        window.alert('ERRO falta dados') 
     }else{
-        alert('tudo certo')
+        res.innerHTML = 'Contando:'
+       let iN = Number(i.value)
+       let fN = Number(f.value)
+       let pN = Number(p.value)
+       if(pN == 0){
+            alert('ERRO')
+       }
+       if(iN < fN){
+            for(let x = iN; x <= fN; x += pN){
+                res.innerHTML += `${x} \u{1F449} ,`
+        }
+       } else{
+        for(let x = iN; x >= fN; x -= pN){
+            res.innerHTML += `${x} \u{1F449} ,`
+        }
+       }
+       res.innerHTML += `\u{1F52E}`
     }
-   
 }
 
 
