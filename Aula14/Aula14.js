bd86acb7529553afbf2e4ad8b8def2632e36b469
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2023-10-30 00:30:51 -0300

    Update Aula14.js

--- Aula14/Aula14.js
+++ Aula14/Aula14.js
@@ -1,4 +1,7 @@
 let num = [1, 3, 6, 8, 9, 3]
+let res = num.sort()
+console.log(res)
+
 for(let pos=0;pos<num.length;pos++){
     console.log(num[pos])
 }
@@ -6,4 +9,5 @@ for(let pos=0;pos<num.length;pos++){
 for(let pos in num){
     console.log(num[pos])
 }
+
 
