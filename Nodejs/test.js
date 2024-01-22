f93dca7438c6000451bec8529c8a744047e9cf80
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2024-01-21 21:23:48 -0300

    Create test.js

---
+++ Nodejs/test.js
@@ -0,0 +1,11 @@
+const pessoa = {
+    nome: "gabriel",
+    idade: 18,
+    gmail: "teste@gmail.com",
+    id: 0
+};
+
+let texto = JSON.stringify(pessoa);
+let obj = JSON.parse(texto)
+console.log(texto);
+console.log(obj.nome)
